22a9ad1df44421769d97840d0ada51eab0f438c7
lienekl <lienekl@yahoo.com>
2026-02-15 14:31:48 +0000

    work on readme

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-import questionary
+import InquirerPy
+from InquirerPy import prompt
 import os
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
 
 
-newpath =  r"C:\Users\liene\bootcamp\challenge-6-bc26\Documents"
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+questions = [
+    {"type": "input", "name": "title", "message": "Project Title:"},
+    {"type": "input", "name": "description", "message": "Project Description:"},
+    {"type": "input", "name": "instructions", "message": "Installation Instructions:"},
+    {"type": "input", "name": "usage", "message": "Usage Information:"},
+    {"type": "list", "name": "license", "message": "Select a License:",  "choices": ["Apache License 2,0", "MIT License", "GNU GPL v3", "GNU LGPL v3", "Mozilla Public License 2.0", "Creative Commons" , "Unlicense",], "default": None,},
+    {"type": "input", "name": "author", "message": "Author:"},
+    {"type": "input", "name": "contact", "message": "Contact Info:"},
+]
+answers = prompt(questions)
 
+def main():
+    answers = inquirer.prompt(questions)
+    print(answers)
+    
+if __name__ == "__main__":
+    main()
 
+data = answers
+def generate_readme(questions, answers):
+    
+    return f """
+# Project Title: {data["title"]}
+
+## Description
+{data["description"]}
+"""
+
+report = generate_readme("Title", "description")
+print(report)
 
 # documents_path = os.path.expanduser("./Documents")
 
